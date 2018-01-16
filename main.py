e43d1c63e8ed018c9b19bda7c82227ea9c60c927
pooi <ldayou@naver.com>
2018-01-16 20:17:47 +0900

    Add feature   * Set the default value when entering a blank value.

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from BingCollector import BingCollector
 
 
 keyword = input("Please input keyword : ")
-max_image = int(input("Maximum number of download images(0:infinity) : "))
-threads = int(input("Number of threads(default is 8) : "))
+try:
+    max_image = int(input("Maximum number of download images(0:infinity) : "))
+except:
+    max_image = 0
+try:
+    threads = int(input("Number of threads(default is 8) : "))
+except:
+    threads = 8
 
 collector = BaiduCollector(num_of_thread=threads)
 collector.collectImage(keyword, max_image)
